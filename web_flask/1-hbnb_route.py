338e2585d73ff184105217c9b8a711306e26f2dc
Mejdoubee <6235@holbertonstudents.com>
2023-08-15 14:14:56 +0100

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
 0-hello_route module
-This module starts a Flask web application with a single route.
+This module starts a Flask web application with a 2 routes.
 The web application listens on 0.0.0.0, port 5000.
 '''
 from flask import Flask
@@ -15,12 +15,14 @@ def hello_HBNB():
     '''
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
     '''
     Returns a string "HBNB" when the route /hbnb is accessed
     '''
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
